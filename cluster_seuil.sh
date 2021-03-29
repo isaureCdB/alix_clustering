febff0a0b98e9f9db2edfdc7f28299e7f7bc8085
CHAUVOT DE BEAUCHENE Isaure <isaure.chauvot-de-beauchene@loria.fr>
2021-03-29 17:15:19 +0200

    rewrite

---
+++ cluster_seuil.sh
@@ -0,0 +1,19 @@
+############################################
+# inputs
+############################################
+# AAA_test_trigo.npy = internal coordinates for subset of AAA fragments
+# AAA_trigo.npy = internal coordinates for all AAA fragments
+# rmsd_AAAr_test.npy = exact pairwise RMSD of subset of AAA fragments
+
+
+# get thresholds of delta in internal coordinate above which a pair has RMSD > 1A.
+# output is a vector of length = number of internal coordinates.
+# !!!! Use on a subset of fragments (< 10.000) !!! 
+get_thresholds.py rmsd_AAAr_test.npy AAA_test_trigo.npy 1 threshold_AAA_test_1A.npy
+
+
+# get boolean mask of which pair has all delta values below thresholds.
+# Use on the full set of fragments.
+filter_intcoor.py AAAr.npy AAA_trigo.npy threshold_AAA_test_1A.npy AAA_trigo_mask.npy
+
+
---
+++ get_thresholds.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+rmsd = np.load(sys.argv[1]) #rmsdAAA_test.npy
+intcoor = np.load(sys.argv[2]) #AAA_test_trigo.npy
+cutoff = float(sys.argv[3]) # 1A RMSD cutoff
+
+nstruc, ncoor = intcoor.shape()
+assert rmsd.shape() == (nstruc, nstruc)
+
+intcoor_diff = np.abs(intcoor[:,None, :]-intcoor[None, :, :]) 
+
+intcoor_diff_inf = intcoor_diff[rmsd < cutoff]
+
+thresholds = np.min(intcoor_diff_inf1)
+
+np.save(sys.argv[4], thresholds)
