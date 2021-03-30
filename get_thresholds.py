f5366408860c77767d138dc8764c3c1ecd15f91a
CHAUVOT DE BEAUCHENE Isaure <isaure.chauvot-de-beauchene@loria.fr>
2021-03-30 12:00:07 +0200

    correct min to max

--- get_thresholds.py
+++ get_thresholds.py
@@ -4,28 +4,31 @@ import sys
 import numpy as np
 
 def get_threshold(rmsd_matrix, intern_coordinate, cutoff):
-    nstruc, ncoor = intern_coordinate.shape()
-    try:
-        assert rmsd_matrix.shape() == (nstruc, nstruc)
+    nstruc, ncoor = intern_coordinate.shape
 
-        intcoor_diff = np.abs(intern_coordinate[:,None, :]-intern_coordinate[None, :, :])
+    assert rmsd_matrix.shape == (nstruc, nstruc)
 
-        intcoor_diff_inf = intcoor_diff[rmsd_matrix < cutoff]
+    intcoor_diff = np.abs(intern_coordinate[:,None, :]-intern_coordinate[None, :, :])
+
+    intcoor_diff_inf = intcoor_diff[rmsd_matrix < cutoff]
+
+    thresholds = np.max(intcoor_diff_inf, axis=0)
+    return thresholds
 
 
 def main():
     rmsd = np.load(sys.argv[1]) #rmsdAAA_test.npy
-    intcoor = np.load(sys.argv[2]) #AAA_test_trigo.npy
+    intern_coordinate = np.load(sys.argv[2]) #AAA_test_trigo.npy
     cutoff = float(sys.argv[3]) # 1A RMSD cutoff
 
-    nstruc, ncoor = intcoor.shape()
-    assert rmsd.shape() == (nstruc, nstruc)
+    nstruc, ncoor = intern_coordinate.shape
+    assert rmsd.shape == (nstruc, nstruc)
 
-    intcoor_diff = np.abs(intcoor[:,None, :]-intcoor[None, :, :])
+    intcoor_diff = np.abs(intern_coordinate[:,None, :]-intern_coordinate[None, :, :])
 
     intcoor_diff_inf = intcoor_diff[rmsd < cutoff]
 
-    thresholds = np.min(intcoor_diff_inf, axis=0)
+    thresholds = np.max(intcoor_diff_inf, axis=0)
 
     np.save(sys.argv[4], thresholds)
 
