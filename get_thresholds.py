01cc781fc4154a9935465e0308adce1449ddf628
antoine_moniot <antoine.moniot@loria.fr>
2021-03-30 10:29:52 +0200

    Change to use as python function

--- get_thresholds.py
+++ get_thresholds.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
 
+import sys
 import numpy as np
 
-rmsd = np.load(sys.argv[1]) #rmsdAAA_test.npy
-intcoor = np.load(sys.argv[2]) #AAA_test_trigo.npy
-cutoff = float(sys.argv[3]) # 1A RMSD cutoff
+def get_threshold(rmsd_matrix, intern_coordinate, cutoff):
+    nstruc, ncoor = intern_coordinate.shape()
+    try:
+        assert rmsd_matrix.shape() == (nstruc, nstruc)
 
-nstruc, ncoor = intcoor.shape()
-assert rmsd.shape() == (nstruc, nstruc)
+        intcoor_diff = np.abs(intern_coordinate[:,None, :]-intern_coordinate[None, :, :])
 
-intcoor_diff = np.abs(intcoor[:,None, :]-intcoor[None, :, :]) 
+        intcoor_diff_inf = intcoor_diff[rmsd_matrix < cutoff]
 
-intcoor_diff_inf = intcoor_diff[rmsd < cutoff]
+        thresholds = np.min(intcoor_diff_inf)
 
-thresholds = np.min(intcoor_diff_inf1)
+        return thresholds
+    except:
+        print("The RMSD matrix has not the size of the number of structures.")
 
-np.save(sys.argv[4], thresholds)
+
+def main():
+    rmsd = np.load(sys.argv[1]) #rmsdAAA_test.npy
+    intcoor = np.load(sys.argv[2]) #AAA_test_trigo.npy
+    cutoff = float(sys.argv[3]) # 1A RMSD cutoff
+
+    nstruc, ncoor = intcoor.shape()
+    assert rmsd.shape() == (nstruc, nstruc)
+
+    intcoor_diff = np.abs(intcoor[:,None, :]-intcoor[None, :, :])
+
+    intcoor_diff_inf = intcoor_diff[rmsd < cutoff]
+
+    thresholds = np.min(intcoor_diff_inf)
+
+    np.save(sys.argv[4], thresholds)
+
+if __name__ == "__main__":
+    main()
